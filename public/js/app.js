d6a1188869ca1c64b402026d2256da7b9c41e846
Luis Torrealba <luis@outsmartlabs.com>
2026-01-29 12:28:28 -0400

    pre-docker

--- public/js/app.js
+++ public/js/app.js
@@ -49418,8 +49418,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! P:\libreria-intelli\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! P:\libreria-intelli\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/luis/Repositories/libreria-intelli/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/luis/Repositories/libreria-intelli/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
